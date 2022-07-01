bb49237ec6090fb2a881765cc7f3f59f15858cef
Ajith Johnson <65328162+Ajithjohnson33@users.noreply.github.com>
2022-07-01 09:31:25 +0530

    build a mini weather App
    if and else concept learned & implemented

---
+++ mini weather App/weatherApp.js
@@ -0,0 +1,18 @@
+
+/*
+ Baby weather app (conditionals)
+ if rain 'grab your umberalla'
+ else 'wear your sunglases'
+*/
+
+let weather = prompt('how is the weather?')
+if (weather == 'rain') {
+    console.log('grab your umberalla');
+} else {
+    console.log('wear your sunglases');
+}
+
+//  conditional operators
+
+
+
